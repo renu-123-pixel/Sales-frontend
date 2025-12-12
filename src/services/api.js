405770d10353120a3d808d95ddcac70d0cb20821
Renu Ekka <114815478+renu-123-pixel@users.noreply.github.com>
2025-12-12 13:43:01 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api", // correct backend port
+  baseURL: import.meta.env.VITE_API + "/api", // correct backend port
 });
 
 // GET SALES WITH QUERY PARAMS
